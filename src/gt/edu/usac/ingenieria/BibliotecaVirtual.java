56cc3c838fada57916bd02e2966de3bbee457231
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2022-02-24 09:06:02 -0600

    Clase con main BibliotecaVirtual

---
+++ src/gt/edu/usac/ingenieria/BibliotecaVirtual.java
@@ -0,0 +1,19 @@
+package gt.edu.usac.ingenieria;
+
+public class BibliotecaVirtual {
+    private void ejecutar() {
+        // TODO Crear usuario admin y la clase usuario
+        // TODO Crear clase libro
+        // TODO Iniciar bucle de creacion de ventanas regresando a esta ventana principal
+        // TODO crear metodos para todas las ventanas
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        new BibliotecaVirtual().ejecutar();
+    }
+}
