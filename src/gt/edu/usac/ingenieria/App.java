d4ba2c26aa0d2bc4ce6ca2a63c8def5a11016b7e
Sebastian Vasquez <sebas.vasquez010@gmail.com>
2022-03-08 20:46:31 -0600

    Codigo limpio de usuarios y libros

--- src/gt/edu/usac/ingenieria/App.java
+++ src/gt/edu/usac/ingenieria/App.java
@@ -6,49 +6,13 @@ import gt.edu.usac.ingenieria.view.PantallaInicioView;
 
 public class App {
     public static void main(String[] args) {
-        // TODO dejar solamente el usuario administrador
-        Usuario[] usuarios = new Usuario[3];
+        Usuario[] usuarios = new Usuario[1];
         usuarios[0] = new Usuario("admin", "", 1, "admin");
         usuarios[0].setPassword("admin");
         usuarios[0].setUser("admin");
-        usuarios[1] = new Usuario("Sebastian", "Vásquez", 3010299380101L, "Estudiante");
-        usuarios[1].setPassword("a");
-        usuarios[1].setUser("zibas");
-        usuarios[2] = new Usuario("Belén", "Izquierdo", 5014896305251L, "Catedrático");
-        usuarios[2].setPassword("belen2004");
-        usuarios[2].setUser("AB");
 
-        Bibliografia[] bibliografias = new Bibliografia[4];
-        bibliografias[0] = new Libro("HP Lovecraft", 1970, "La llamada de Cuthulu",
-                new String[]{"Terror Cosimico", "Simon"}, "Pulpo qliao come gente",
-                new String[]{"Homosexualidad", "Comunismo", "Como cocinar un pastel 3 leches"}, 0, 20,
-                655453);
-        bibliografias[1] = new LibroDigital(
-                "No se lol MIT",
-                1992, "Structure and interpretation of computer programs",
-                new String[]{"Matematica", "Dolor de cabezaaaa"},
-                "Libor re piola con un monton de cosas piolas",
-                new String[]{"Recursion", "Cocinar helados"},
-                5, 557336);
-        bibliografias[2] = new LibroDigital(
-                "MIT",
-                1992, "Structure and interpretation of computer programs 2",
-                new String[]{"Matematica", "Dolor de cabezaaaa"},
-                "Libor re piola con un monton de cosas piolas",
-                new String[]{"Recursion", "Cocinar helados"},
-                3, 557);
-        bibliografias[3] = new Revista(
-                "Algun maje loco",
-                1987,
-                "Un libro loco sobre el amor, la guerra y recetas de cocina",
-                new String[]{"Cocina", "Amor", "Guerra"},
-                "Es solo un libro sobre como hacer tamales, no hay pdo",
-                new String[] {"Cocina", "Tamales", "Depresion"},
-                1,
-                "Cientifico",
-                5,
-                45454545
-        );
+        Bibliografia[] bibliografias = new Bibliografia[0];
+
 
         PantallaInicioView view = new PantallaInicioView();
 
